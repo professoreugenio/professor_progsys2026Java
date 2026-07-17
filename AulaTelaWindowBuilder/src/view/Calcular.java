1191b073250bd9f2d9d33675efef6bd42cb4dc18
professoreugenio <professoreugeniomls@gmail.com>
2026-07-17 07:52:38 -0300

    calcular

--- AulaTelaWindowBuilder/src/view/Calcular.java
+++ AulaTelaWindowBuilder/src/view/Calcular.java
@@ -1,15 +1,27 @@
 package view;
 
 import java.awt.EventQueue;
+import java.math.BigDecimal;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 public class Calcular extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtValor;
+	private JList<String> listaValores;
+
+	private BigDecimal totalAcumulado = BigDecimal.ZERO;
+	private DefaultListModel<String> modeloValores = new DefaultListModel<>();
 
 	/**
 	 * Launch the application.
@@ -31,12 +43,34 @@ public class Calcular extends JFrame {
 	 * Create the frame.
 	 */
 	public Calcular() {
+		setTitle("Calcular despesas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(30, 21, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		txtValor = new JTextField();
+		txtValor.setBounds(30, 36, 159, 30);
+		contentPane.add(txtValor);
+		txtValor.setColumns(10);
+		
+		JButton btSomar = new JButton("New button");
+		btSomar.setBounds(199, 36, 89, 30);
+		contentPane.add(btSomar);
+		
+		JLabel lblValoreAdicionados = new JLabel("Valore adicionados");
+		lblValoreAdicionados.setBounds(30, 78, 134, 14);
+		contentPane.add(lblValoreAdicionados);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(30, 103, 276, 135);
+		contentPane.add(scrollPane);
 
 	}
-
 }
--- AulaTelaWindowBuilder/src/view/TelaCadastroSimples.java
+++ AulaTelaWindowBuilder/src/view/TelaCadastroSimples.java
@@ -53,66 +53,65 @@ public class TelaCadastroSimples extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Cadastro Simples de Produto");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblNewLabel.setBounds(122, 11, 239, 17);
 		contentPane.add(lblNewLabel);
-		
+
 		JLabel lbProduto = new JLabel("Produto");
 		lbProduto.setHorizontalAlignment(SwingConstants.RIGHT);
 		lbProduto.setBounds(49, 75, 70, 14);
 		contentPane.add(lbProduto);
-		
+
 		JLabel lblValor = new JLabel("Valor");
 		lblValor.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblValor.setBounds(49, 124, 70, 14);
 		contentPane.add(lblValor);
-		
+
 		JLabel lblQuantidade = new JLabel("Quantidade");
 		lblQuantidade.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblQuantidade.setBounds(49, 171, 70, 14);
 		contentPane.add(lblQuantidade);
-		
+
 		txtProduto = new JTextField();
 		txtProduto.setToolTipText("Digite o nome do produto");
 		txtProduto.setBounds(129, 70, 220, 25);
 		contentPane.add(txtProduto);
 		txtProduto.setColumns(10);
-		
+
 		txtValor = new JTextField();
 		txtValor.setColumns(10);
 		txtValor.setBounds(129, 119, 100, 25);
 		contentPane.add(txtValor);
-		
+
 		txtQuantidade = new JTextField();
 		txtQuantidade.setColumns(10);
 		txtQuantidade.setBounds(129, 166, 100, 25);
 		contentPane.add(txtQuantidade);
-		
+
 		JButton btCadastrar = new JButton("Cadastrar");
-		
+
 		btCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
 				exibir();
-				
+
 			}
 		});
 		btCadastrar.setBounds(129, 222, 91, 25);
 		contentPane.add(btCadastrar);
-		
+
 		JButton btLimpar = new JButton("Limpar");
 		btLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				limpar();
-				
+
 			}
 		});
 		btLimpar.setBounds(228, 222, 91, 25);
 		contentPane.add(btLimpar);
-		
+
 		JButton btSair = new JButton("Sair");
 		btSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -124,29 +123,30 @@ public class TelaCadastroSimples extends JFrame {
 		setLocationRelativeTo(null);
 
 	}
-	
+
 	public void exibir() {
-		
+
 		String produto = txtProduto.getText().trim();
 		String quantidade = txtQuantidade.getText().trim();
 		String valor = txtValor.getText();
-		
-		msg += produto+"\n";
-		msg += quantidade+"\n";
-		msg += valor+"\n";
-		
+
+		msg += produto + "\n";
+		msg += quantidade + "\n";
+		msg += valor + "\n";
+
 		JOptionPane.showMessageDialog(null, msg);
 	}
-	
+
 	public void sair() {
+		int confirma = JOptionPane.showConfirmDialog(txtProduto, contentPane)
 		System.exit(0);
 	}
+
 	public void limpar() {
 		txtProduto.setText("");
 		txtQuantidade.setText("");
 		txtValor.setText("");
 
 	}
-	
-	
+
 }
