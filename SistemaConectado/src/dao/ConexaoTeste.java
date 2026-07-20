62b75e8b5511c32b027b0b110038587c6157b42d
devprofessoreugenio <dev.professoreugeniomls@gmail.com>
2026-07-20 00:36:36 -0300

    atualização

---
+++ SistemaConectado/src/dao/ConexaoTeste.java
@@ -0,0 +1,49 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoTeste {
+	
+	 private static final String URL =
+	            "jdbc:mysql://localhost:3307/controle_estoque_db";
+
+	    private static final String USUARIO = "root";
+
+	    private static final String SENHA = "&ugML$2834&2026";
+
+	    public static Connection conectar() throws SQLException {
+
+	        return DriverManager.getConnection(
+	                URL,
+	                USUARIO,
+	                SENHA
+	        );
+	    }
+
+	    public static void main(String[] args) {
+
+	        try (Connection conexao = ConexaoTeste.conectar()) {
+
+	            System.out.println("Conexão realizada com sucesso!");
+
+	            System.out.println(
+	                "Banco conectado: " + conexao.getCatalog()
+	            );
+
+	        } catch (SQLException erro) {
+
+	            System.out.println("Erro ao conectar com o banco de dados.");
+
+	            System.out.println(
+	                "Mensagem: " + erro.getMessage()
+	            );
+
+	            System.out.println(
+	                "Código do erro: " + erro.getErrorCode()
+	            );
+	        }
+	    }
+
+}
